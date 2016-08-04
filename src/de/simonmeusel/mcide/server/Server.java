b16d6c55c9b9389fa6270d09f03f803b57307329
Simon Meusel <simon.meuse.pc@t-online.de>
2016-08-04 23:26:57 +0200

    Add ssl support

--- src/de/simonmeusel/mcide/server/Server.java
+++ src/de/simonmeusel/mcide/server/Server.java
@@ -1,9 +1,16 @@
 package de.simonmeusel.mcide.server;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.security.KeyStore;
+
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLServerSocket;
+import javax.net.ssl.SSLServerSocketFactory;
+import javax.net.ssl.SSLSocket;
 
 import org.bukkit.Bukkit;
 
@@ -11,23 +18,31 @@ import de.simonmeusel.mcide.plugin.Plugin;
 
 public class Server {
 
-	ServerSocket server;
+	SSLServerSocket server;
 	public int serverTask;
 
 	@SuppressWarnings("deprecation")
-	public Server(Plugin plugin) {
+	public Server(Plugin plugin, String password) {
 		serverTask = Bukkit.getScheduler().scheduleAsyncDelayedTask(plugin, new Runnable() {
 
 			@Override
 			public void run() {
 				try {
-					
+					KeyStore ks = KeyStore.getInstance("JKS");
+					ks.load(new FileInputStream(new File(plugin.getDataFolder(), "keystore")), password.toCharArray());
+					KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
+					kmf.init(ks, password.toCharArray());
+					SSLContext sc = SSLContext.getInstance("TLS");
+					sc.init(kmf.getKeyManagers(), null, null);
+					SSLServerSocketFactory ssf = sc.getServerSocketFactory();
+					server = (SSLServerSocket) ssf.createServerSocket(25564);
+
 					System.out.println("Mcide Server started!");
 
 					while (!server.isClosed()) {
 						try {
-							Socket client = server.accept();
-							
+							SSLSocket client = (SSLSocket) server.accept();
+
 							String address = client.getLocalAddress().getHostAddress();
 							System.out.println("Mcide accepted a client from " + address + "!");
 
